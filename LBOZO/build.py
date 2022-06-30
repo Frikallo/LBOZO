71f3efe5cdc75bb8cf8cfb73a5fb0f1a17488cee
Frikallo <noahskay@icloud.com>
2022-06-30 02:40:32 -0700

    --

--- LBOZO/build.py
+++ LBOZO/build.py
@@ -1,24 +1,28 @@
 import base64, os, sys
 
+
 def error(s):
     print(s)
     sys.exit(-1)
 
 
 def build(program):
-    command = 'python3 -m PyInstaller -F --clean LBOZO/src/main.py -n {}'.format(program)
+    command = "python3 -m PyInstaller -F --clean LBOZO/src/main.py -n {}".format(
+        program
+    )
     os.system(command)
 
     try:
-        with open('dist/{}.exe'.format(program), 'rb') as f:
+        with open("dist/{}.exe".format(program), "rb") as f:
             ret = f.read()
             output64 = base64.b64encode(ret)
     except:
-        error('{} binary doesnt exist, compilation failed.'.format(program))
+        error("{} binary doesnt exist, compilation failed.".format(program))
 
-    with open('dist/base64{}'.format(program), 'wb') as f:
+    with open("dist/base64{}".format(program), "wb") as f:
         f.write(output64)
-    
+
     return output64
 
-build('LBOZO')
+
+build("LBOZO")
